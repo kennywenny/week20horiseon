c6b8e7f32876fa6da617736fbb87adb3f942caa1
Kenny Hung (he) <khung@instructors.2u.com>
2023-02-16 21:08:58 +0000

    Header as component

---
+++ src/component/header.js
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+      <div class="header">
+        <h1>Hori<span class="seo">seo</span>n</h1>
+        <div>
+          <ul>
+            <li>
+              <a href="#search-engine-optimization">Search Engine Optimization</a>
+            </li>
+            <li>
+              <a href="#online-reputation-management">Online Reputation Management</a>
+            </li>
+            <li>
+              <a href="#social-media-marketing">Social Media Marketing</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+  )
+}
